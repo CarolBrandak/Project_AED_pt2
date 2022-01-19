faa46f0d541db09a46fbc1b4d44174a08d1855e1
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-19 11:05:35 +0000

    drafts

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include "src/STCP.cpp"
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    STCP stcp = STCP();
     return 0;
 }
--- src/STCP.h
+++ src/STCP.h
@@ -1,13 +1,12 @@
 #ifndef PROJECT_AED_PT2_STCP_H
 #define PROJECT_AED_PT2_STCP_H
 
-// Uma classe de dados. Contém todos os grafos, leitores, e métodos de search / comparação
-// O menu tem acesso aos métodos e para cada case: STCP.getDistance("FEUP", "FEP") ou por coordenadas ou por aí vai
+#define STOPS "data/stops.csv"
 
 #include <iostream>
 #include <string>
 #include <fstream>
-#include "Graph.h"
+#include "Graph.cpp"
 
 class STCP {
 
