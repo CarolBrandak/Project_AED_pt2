3a6331f966a759ec664199cf6f9fc185de5b2302
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-20 00:51:37 +0000

    Added initial documentation

---
+++ documentation/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['getmenu_0',['getMenu',['../class_menu.html#a7332b06cbcb8399b2af6e0184fc5ef77',1,'Menu']]],
+  ['graph_1',['Graph',['../class_graph.html',1,'']]]
+];
---
+++ documentation/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['getmenu_0',['getMenu',['../class_menu.html#a7332b06cbcb8399b2af6e0184fc5ef77',1,'Menu']]]
+];
